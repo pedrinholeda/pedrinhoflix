947720a29af143d6ae568446a6a437cb9658c04c
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-07-28 18:31:22 -0300

    deploy first version

---
+++ build/precache-manifest.939f15caab11fd1782344fb4790a1562.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7943c834b5219531c887e67283249c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e449237ff07fa2abe723",
+    "url": "/static/css/main.137d3fab.chunk.css"
+  },
+  {
+    "revision": "727ca583c1ed5a2cd75f",
+    "url": "/static/js/2.a95a2075.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a95a2075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e449237ff07fa2abe723",
+    "url": "/static/js/main.5fd38eeb.chunk.js"
+  },
+  {
+    "revision": "2b6703978428c99897b8",
+    "url": "/static/js/runtime-main.b09f2dd5.js"
+  },
+  {
+    "revision": "bb0e9e3cd8ff965f08d4113b05314055",
+    "url": "/static/media/Logo.bb0e9e3c.png"
+  },
+  {
+    "revision": "77d45ab56206c6db468d81b17a511b8f",
+    "url": "/static/media/pedro.77d45ab5.png"
+  }
+]);
